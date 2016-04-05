bf0c7db47f9b7f9f8f1ff5bc0946c72985e276b3
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2016-04-05 12:38:28 +0100

    More testing. Prepping to restructure project.

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -45,17 +45,18 @@ angular.module('controllers', ['firebase'])
 }])
 
 .factory("Items", function($firebaseArray) {
-	var itemsRef = new Firebase("");
+	var itemsRef = new Firebase("https://resplendent-inferno-6066.firebaseio.com/0");
+	console.log("Test");
 	return $firebaseArray(itemsRef);
 })
 
 .controller('StaffController', ['$scope', '$http', '$state',
 	function($scope, $http, $state, Items) {
-	$http.get('js/staff.json').success(function(data) {
+	// $http.get('js/staff.json').success(function(data) {
 	// $http.get('https://resplendent-inferno-6066.firebaseio.com').success(function(data) {
-		$scope.stafflist = data;
+		$scope.stafflist = Items;
 		$scope.whichstaff = $state.params.detail;
-	});
+	// });
 }])
 
 .controller('QuizController', ['$scope', function($scope) {
