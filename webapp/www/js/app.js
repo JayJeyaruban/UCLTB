a743b65dc5e58cc0d4df36668d9cac20af3e6a34
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2016-04-12 20:47:12 +0100

    Added login.js

--- webapp/www/js/app.js
+++ webapp/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('app', ['ionic'])
+var app = angular.module('app', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,14 @@ angular.module('app', ['ionic'])
     }
   });
 })
+
+.config(['$routerProvider', function($routerProvider) {
+	$routerProvider
+		.when('/login', {
+			templateUrl: 'views/login.html',
+			controller: 'LoginCtrl'
+		})
+		.otherwise({
+			redirectTo: '/login'
+		})
+}])
