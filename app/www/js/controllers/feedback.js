4926a9c896493c9c27630060f9e8bb1237da6624
Pius <08pjude@gmai.com>
2016-04-12 15:02:25 +0530

    change in title of feedback page

--- app/www/js/controllers/feedback.js
+++ app/www/js/controllers/feedback.js
@@ -1,7 +1,5 @@
 angular.module('app')
 
-.controller('FeedbackController', ['$scope', '$firebaseArray', '$state',
-	function($scope, $firebaseArray, $state) {
-	$scope.faq = $firebaseArray(firebase.child("faq"));
-	$scope.whichasked = $state.params.asked;
-}])
+.controller('FeedbackController', ['$scope', function($scope) {
+
+}]);
