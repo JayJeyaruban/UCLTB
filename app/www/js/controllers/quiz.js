343b822c02a8a03d088d09b3870e21c98f2b7ade
Pius <08pjude@gmai.com>
2016-04-13 15:25:19 +0530

    popup added to quiz and is linked to firebase

--- app/www/js/controllers/quiz.js
+++ app/www/js/controllers/quiz.js
@@ -1,7 +1,32 @@
 angular.module('app')
 
-.controller('QuizController', ['$scope', '$firebaseArray', '$state',
-	function($scope, $firebaseArray, $state) {
+.controller('QuizController', ['$scope', '$firebaseArray', '$state','$ionicPopup',
+	function($scope, $firebaseArray, $state, $ionicPopup) {
 	$scope.quiz = $firebaseArray(firebase.child("quiz"));
 	$scope.whichquestion = $state.params.question;
+	$scope.correctAlert = function() {
+	
+    var alertPopup = $ionicPopup.alert({
+         title: 'Correct',
+         template: '<p align="center">Well done. Answer is correct</p>'
+      });
+
+    alertPopup.then(function(res) {
+         console.log('Thank you for attempting an answer');
+     });
+
+   };
+    $scope.incorrectAlert = function() {
+	
+    var alertPopup = $ionicPopup.alert({
+         title: 'Incorrect',
+         template: '<p align="center">Answer is Incorrect</p>'
+      });
+
+    alertPopup.then(function(res) {
+         console.log('Thank you for attempting an answer');
+     });
+
+   };
+	
 }])
