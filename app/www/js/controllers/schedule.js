dacca1dce81ec5990c797428a7a442e0cec7b168
Pius <08pjude@gmai.com>
2016-04-18 16:02:20 +0100

    Schedule page now uploads from firebase with customized css

--- app/www/js/controllers/schedule.js
+++ app/www/js/controllers/schedule.js
@@ -1,5 +1,7 @@
 angular.module('app')
 
-.controller('ScheduleController', ['$scope', function($scope) {
-
+.controller('ScheduleController', ['$scope', '$firebaseArray', '$state',
+	function($scope, $firebaseArray, $state) {
+	$scope.schedule = $firebaseArray(firebase.child("schedule"));
+	$scope.whichschedule = $state.params.schedule;
 }])
