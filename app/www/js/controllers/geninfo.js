41c3b68cd9d7f47f3aa5c3550fc0936c3a228aff
Pius <08pjude@gmai.com>
2016-04-21 21:01:03 +0100

    Added tabs to geninfo page and uploads from firebase

--- app/www/js/controllers/geninfo.js
+++ app/www/js/controllers/geninfo.js
@@ -1,6 +1,7 @@
 angular.module('app')
 
-.controller('GenInfoController', ['$scope', '$firebaseObject',
-	function($scope, $firebaseObject) {
-	$scope.about = $firebaseObject(firebase);
+.controller('GenInfoController', ['$scope', '$firebaseArray', '$state',
+	function($scope, $firebaseArray, $state) {
+	$scope.info = $firebaseArray(firebase.child("About TB"));
+	$scope.whichinfo = $state.params.info;
 }])
