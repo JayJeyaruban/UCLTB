36e15bc4767741076220fdea1be8d19072bb17a0
Pius <08pjude@gmai.com>
2016-04-23 14:32:53 +0100

    Controller updated for staff

--- app/www/js/controllers/staff.js
+++ app/www/js/controllers/staff.js
@@ -6,9 +6,19 @@ angular.module('app')
 // 	$scope.stafflist = $firebaseArray(ref);
 // }])
 
-.controller('StaffController', ['$scope', '$firebaseArray', '$state', function($scope, $firebaseObject, $state) {
+/*.controller('StaffController', ['$scope', '$firebaseArray', '$state', function($scope, $firebaseObject, $state) {
 	var ref = new Firebase("https://resplendent-inferno-6066.firebaseio.com/");
 
 	$scope.stafflist = $firebaseObject(firebase.child("staff"));
 	$scope.whichstaff = $state.params.detail;
+}])*/
+
+.controller('StaffController', ['$scope', '$firebaseArray', '$state',
+	function($scope, $firebaseArray, $state) {
+	$scope.staff = $firebaseArray(firebase.child("staff"));
+	$scope.whichdetail = $state.params.detail;
+	
 }])
+
+
+
