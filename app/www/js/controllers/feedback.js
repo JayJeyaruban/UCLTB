ab7ba603faea59571d403b18975daaf20776b3d4
Pius <08pjude@gmai.com>
2016-04-26 10:03:07 +0100

    made changes to feedback.js so input in prompt passes to firebase

--- app/www/js/controllers/feedback.js
+++ app/www/js/controllers/feedback.js
@@ -1,5 +1,55 @@
 angular.module('app')
 
-.controller('FeedbackController', ['$scope', function($scope) {
+/*.controller('FeedbackController', ['$scope', function($scope) {
 
-}]);
+}]);*/
+
+
+
+.factory('Items', ['$firebaseArray', function($firebaseArray) {
+  var itemsRef = new Firebase('https://ucltb.firebaseio.com/items/comments');
+  return $firebaseArray(itemsRef);
+}])
+
+.controller("FeedbackController", function($scope, Items) {
+
+  $scope.comments = Items;
+
+  $scope.addComment = function() {
+    var name = prompt("What would you like to comment?");
+    if (name) {
+      $scope.comments.$add({
+        'comment': name
+      });
+    }
+  };
+  
+  
+ 
+});
+
+
+
+
+
+/*.factory('Items', ['$firebaseArray', function($firebaseArray) {
+  var itemsRef = new Firebase('https://ucltb.firebaseio.com/items');
+  return $firebaseArray(itemsRef);
+}])
+
+.controller("FeedbackController", function($scope, Items) {
+
+  $scope.items = Items;
+
+  $scope.addItem = function() {
+    var name = prompt("What would you like to comment?");
+    if (name) {
+      $scope.items.$add({
+        'name': name
+      });
+    }
+  };
+  
+  
+ 
+});*/
